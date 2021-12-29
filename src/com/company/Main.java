d3034af0b6d1eb896338fd93245ddc845bb87080
Roman Safin <mrromasa@gmail.com>
2021-12-29 21:20:29 +0300

    added add method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
-package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("peace");
+        Song song1 = new Song(1988, "Smooth Criminal");
+        Song song2 = new Song(1981, "In The Air Tonight");
+        Song song3 = new Song(1967, "Nights in White Satin");
+        MyArrayList<Song> songs = new MyArrayList<Song>();
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3, 1);
+        System.out.println(Arrays.toString(songs.getObjects()));
     }
+
 }
