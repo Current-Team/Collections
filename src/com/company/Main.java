8c85af057b5a38aeebf04679719aa2f1833c8d35
Roman Safin <mrromasa@gmail.com>
2022-01-02 17:53:49 +0300

    add sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
-package com.company;
+import model.MyArrayList;
+import model.Song;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("peace");
+        Song song1 = new Song(1988, "Smooth Criminal");
+        Song song2 = new Song(1981, "In The Air Tonight");
+        Song song3 = new Song(1967, "Nights in White Satin");
+        MyArrayList<Song> songs = new MyArrayList<Song>();
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3, 1);
+        songs.sort();
+        List<Object> songList = new ArrayList<>();
+
+        System.out.println(Arrays.toString(songs.getObjects()));
     }
 }
---
+++ src/com/company/service/List.java
@@ -0,0 +1,11 @@
+package service;
+
+public interface List<T> {
+    void add(T element);
+    void add(T element, int index);
+    T get(int index);
+    int getIndex(T object) throws RuntimeException;
+    void delete(int index);
+    int size();
+    void sort();
+}
